61ee21dc1caee9009b2694af1718f7945fdab5f2
Juuso Lappalainen <juuso.u.lappalainen@gmail.com>
2022-02-04 22:12:39 +0100

    päivitellääs sääntöjä

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,49 +15,18 @@ const Home: NextPage = () => {
 				<h1 className={styles.title}>SKI EXTREME</h1>
 				<h2>MANIFESTO</h2>
 				<ol className={styles.manifesto}>
-					<li>
-						Kaikki, jotka eivät laske frame-siteillä ovat{' '}
-						<a href="https://fi.linkedin.com/in/luukascastren">nössöjä kanankoipia</a>
-					</li>
-					<li>Joka kolmas päivä voi olla krapula</li>
+					<li>Kaikki, jotka eivät laske frame-siteillä ovat nössöjä kanankoipia</li>
+					<li>Joka kolmas päivä voi juoda kaljaa</li>
 					<li>Jos et ikinä kaadu, et yritä tarpeeksi</li>
 					<li>Jos kaadut liian usein, polta suksesi tynnyrissä</li>
 					<li>Lounas syödään laitoksessa (EI à la carte)</li>
-					<li>Red panache on jumalten juoma</li>
-					<li>
-						Erimielisyydet ratkaistaan{' '}
-						<a href="https://www.youtube.com/watch?v=_Qe3HBqFhTU">Kiinalaisella alamäellä</a>
-					</li>
-					<li>
-						Jokaisella reissulla katsotaan Apocalypse Snow{' '}
-						<a
-							target="_blank"
-							href="https://www.youtube.com/watch?v=rbdUEOyryeg"
-							rel="noopener noreferrer"
-						>
-							I
-						</a>
-						,{' '}
-						<a
-							target="_blank"
-							href="https://www.youtube.com/watch?v=pdO-jllFqmA"
-							rel="noopener noreferrer"
-						>
-							II
-						</a>{' '}
-						ja{' '}
-						<a
-							target="_blank"
-							href="https://www.youtube.com/watch?v=n4A7ssN2xy0"
-							rel="noopener noreferrer"
-						>
-							III
-						</a>
-					</li>
+					<li>Lempijuomasi on Red Panaché</li>
+					<li>Erimielisyydet ratkaistaan Kiinalaisella alamäellä</li>
+					<li>Jokaisella reissulla katsotaan Apocalypse Snow I, II, III</li>
 					<li>Ei lautailijoita</li>
 					<li>
-						Jos käyttäydyt Manifeston vastaisesti, otetaan suksesi takavarikkoon (mutta ensin,{' '}
-						<a href="https://www.youtube.com/watch?v=RxIzX2UglpA">chi-chi</a>)
+						Jos käyttäydyt Manifeston vastaisesti, otetaan suksesi takavarikkoon (mutta ensin,
+						chi-chi)
 					</li>
 				</ol>
 			</main>
