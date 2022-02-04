cac1e7e7926bc07fd44d44690b05b4a293ccaa7e
Pekka Lammi <haloo@pekkalammi.com>
2022-02-05 00:26:32 +0100

    parallax mvp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,16 @@
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
-import { Box, ChakraProvider } from "@chakra-ui/react";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Box, ChakraProvider } from '@chakra-ui/react';
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <ChakraProvider>
-      <Box
-        w="100vw"
-        h="70vh"
-        zIndex="-1"
-        position="absolute"
-        bg="blue.50"
-      ></Box>
-      <Box
-        w="100vw"
-        h="30vh"
-        top="70vh"
-        zIndex="-1"
-        position="absolute"
-        bgGradient={["linear(to-b, blue.50, blue.200)"]}
-      ></Box>
-
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider>
+			<ParallaxProvider>
+				<Component {...pageProps} />
+			</ParallaxProvider>
+		</ChakraProvider>
+	);
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,118 @@
+import { Box, Heading } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import { Parallax, useParallax } from 'react-scroll-parallax';
 import styles from '../styles/Home.module.css';
 
 const Home: NextPage = () => {
 	return (
-		<div className={styles.container}>
+		<Box h="100vh">
 			<Head>
 				<title>Ski Extreme</title>
 				<meta name="description" content="For the believeable ski heros who ski the exreme" />
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
-			<main className={styles.main}>
-				<h1 className={styles.title}>SKI EXTREME</h1>
-				<h2>MANIFESTO</h2>
-				<ol className={styles.manifesto}>
-					<li>Kaikki, jotka eivät laske frame-siteillä ovat nössöjä kanankoipia</li>
-					<li>Joka kolmas päivä voi juoda kaljaa</li>
-					<li>Jos et ikinä kaadu, et yritä tarpeeksi</li>
-					<li>Jos kaadut liian usein, polta suksesi tynnyrissä</li>
-					<li>Lounas syödään laitoksessa (EI à la carte)</li>
-					<li>Lempijuomasi on Red Panaché</li>
-					<li>Erimielisyydet ratkaistaan Kiinalaisella alamäellä</li>
-					<li>Jokaisella reissulla katsotaan Apocalypse Snow I, II, III</li>
-					<li>Ei lautailijoita</li>
-					<li>
-						Jos käyttäydyt Manifeston vastaisesti, otetaan suksesi takavarikkoon (mutta ensin,
-						chi-chi)
-					</li>
-				</ol>
-			</main>
-		</div>
+			<Box position="relative" zIndex={2} height="100vh" width="100vw">
+				<img
+					style={{ position: 'relative', height: '100vh', width: '100vw' }}
+					src="https://assets.codepen.io/721952/sky.jpg"
+				/>
+				<Parallax speed={20}>
+					<img
+						className={styles.mtnPic}
+						style={{ position: 'relative' }}
+						src="https://assets.codepen.io/721952/mountBg.png"
+					/>
+				</Parallax>
+				<Parallax speed={50}>
+					<img
+						className={styles.mtnPic}
+						style={{ position: 'relative' }}
+						src="https://assets.codepen.io/721952/mountMg.png"
+						alt="mountMg"
+					/>
+				</Parallax>
+				<Parallax speed={100}>
+					<img
+						className={styles.mtnPic}
+						src="https://assets.codepen.io/721952/mountFg.png"
+						alt="mountFg"
+					/>
+				</Parallax>
+				<Parallax speed={200}>
+					<img
+						className={styles.mtnPic}
+						src="https://assets.codepen.io/721952/cloud1.png"
+						alt="mountFg"
+					/>
+				</Parallax>
+				<Parallax speed={150}>
+					<img
+						className={styles.mtnPic}
+						src="https://assets.codepen.io/721952/cloud2.png"
+						alt="mountFg"
+					/>
+				</Parallax>
+				<Parallax translateY={150}>
+					<img
+						className={styles.mtnPic}
+						src="https://assets.codepen.io/721952/cloud3.png"
+						alt="mountFg"
+					/>
+				</Parallax>
+			</Box>
+			<Box position="absolute" top={0} zIndex={15} w="100%" textAlign="center">
+				<Heading size="4xl" color="white" mt={8}>
+					SKI EXTREME
+				</Heading>
+			</Box>
+			<Parallax speed={200} style={{ position: 'relative', zIndex: 15 }}>
+				<Box height="100vh" w="100vw" bg="white" mt="-75.5vh">
+					<main className={styles.main}>
+						<h2>MANIFESTO</h2>
+						<ol className={styles.manifesto}>
+							<li>Kaikki, jotka eivät laske frame-siteillä ovat nössöjä kanankoipia</li>
+							<li>Joka kolmas päivä voi juoda kaljaa</li>
+							<li>Jos et ikinä kaadu, et yritä tarpeeksi</li>
+							<li>Jos kaadut liian usein, polta suksesi tynnyrissä</li>
+							<li>Lounas syödään laitoksessa (EI à la carte)</li>
+							<li>Lempijuomasi on Red Panaché</li>
+							<li>Erimielisyydet ratkaistaan Kiinalaisella alamäellä</li>
+							<li>Jokaisella reissulla katsotaan Apocalypse Snow I, II, III</li>
+							<li>Ei lautailijoita</li>
+							<li>
+								Jos käyttäydyt Manifeston vastaisesti, otetaan suksesi takavarikkoon (mutta ensin,
+								chi-chi)
+							</li>
+						</ol>
+					</main>
+				</Box>
+			</Parallax>
+			<Parallax translateY={[200, -200]}>
+				<Box height="100vh" w="100vw" bg="white">
+					<main className={styles.main}>
+						<h1 className={styles.title}>SKI EXTREME</h1>
+						<h2>MANIFESTO</h2>
+						<ol className={styles.manifesto}>
+							<li>Kaikki, jotka eivät laske frame-siteillä ovat nössöjä kanankoipia</li>
+							<li>Joka kolmas päivä voi juoda kaljaa</li>
+							<li>Jos et ikinä kaadu, et yritä tarpeeksi</li>
+							<li>Jos kaadut liian usein, polta suksesi tynnyrissä</li>
+							<li>Lounas syödään laitoksessa (EI à la carte)</li>
+							<li>Lempijuomasi on Red Panaché</li>
+							<li>Erimielisyydet ratkaistaan Kiinalaisella alamäellä</li>
+							<li>Jokaisella reissulla katsotaan Apocalypse Snow I, II, III</li>
+							<li>Ei lautailijoita</li>
+							<li>
+								Jos käyttäydyt Manifeston vastaisesti, otetaan suksesi takavarikkoon (mutta ensin,
+								chi-chi)
+							</li>
+						</ol>
+					</main>
+				</Box>
+			</Parallax>
+		</Box>
 	);
 };
 
