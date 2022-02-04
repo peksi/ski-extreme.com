d09bff4475d97eb4bde4b0bb4843a9b0bcc353a3
Pekka Lammi <haloo@pekkalammi.com>
2022-02-04 20:46:06 +0100

    include styles, remove footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,19 +34,6 @@ const Home: NextPage = () => {
           <li>Mutta ensin, chi-chi</li>
         </ol>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
