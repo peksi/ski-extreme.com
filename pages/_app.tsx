7f143d302c7ea2003eb063c6689afe29cd0b4234
Pekka Lammi <haloo@pekkalammi.com>
2022-02-04 20:58:18 +0100

    some background

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,26 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Box
+        w="100vw"
+        h="70vh"
+        zIndex="-1"
+        position="absolute"
+        bg="blue.100"
+      ></Box>
+      <Box
+        w="100vw"
+        h="30vh"
+        top="70vh"
+        zIndex="-1"
+        position="absolute"
+        bgGradient={["linear(to-b, blue.100, blue.300)"]}
+      ></Box>
+
       <Component {...pageProps} />
     </ChakraProvider>
   );
